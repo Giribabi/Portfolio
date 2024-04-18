afd0a0726a4fbe1e3211f1ef2a78220e55b4385a
Giribabi <sammidisaroja@gmail.com>
2024-04-19 00:35:48 +0530

    Updated my Projects

--- src/components/About/Projects/Projects.jsx
+++ src/components/About/Projects/Projects.jsx
@@ -9,30 +9,34 @@ function Projects() {
 
     const projects = [
         {
-            name: "ShopEasy",
-            index: 1,
-            content: `<p>This is an front-end project of an e-commerce platform and is completely designed in react.js.<ul> <li> The login username and password can be found in dummyjson docs. </li><li>I have already set the login credentials for direct login.</li><li>We must remember that accurate login credentials are necessary to enter into the home page. </li><li>After the login authentication, you will be redirected to the protected Home page. </li><li>Then you can search for the available products. </li><li>You can filter the products according to their price. </li><li>You can view the number of items added to cart at the top of the cart icon. </li><li>The total cost of the items in the cart is found below the cart icon. </li><li>You can view the cart by clicking on the cart icon. </li><li>You can also tap on any item card on the home page to view its complete description. </li><li>At last you can logout from the session by clicking the logout button. <li>I have collected and used all the data from <a href="https://dummyjson.com/docs" target="blank">here</a></li></ul></p>`,
-            github_link: "https://github.com/Giribabi/ShopEasy",
-            deployment_link: "https://shop-easy-updated.vercel.app/",
+            name: "Data Visualization",
+            index: 3,
+            content: `<p>This is a MERN stack project in which the data must be fetched from the database according to the filters query. The data must be visualized in the form of charts. <ul><li>I have used ChartJs for the purpose of data visualization.</li></p>`,
+            github_link:
+                "https://github.com/Giribabi/BlackcofferDataVisualization/",
+            deployment_link:
+                "https://blackcoffer-data-visualization.vercel.app/",
         },
         {
             name: "My Portfolio",
             index: 2,
-            content: `<p>You are watching it now.<ul><li>It is built using react.js </li><li>This project showcases my frontend skills.</li><li>For easy maintainance of the CSS code, I have written the CSS selectors in ascending order.</li><li>I have also used 'dangerouslySetInnerHTML' property here.</li></ul></p>`,
+            content: `<p>You are watching it now.<ul><li>It is built using ReactJs </li><li>This project showcases my frontend skills.</li><li>For easy maintainance of the CSS code, I have written the CSS selectors in ascending order.</li><li>I have also used 'dangerouslySetInnerHTML' property here.</li></ul></p>`,
             github_link: "https://github.com/Giribabi/ShopEasy",
             deployment_link: "#home-section",
+            ppt_link:
+                "https://www.canva.com/design/DAGBi8OQfQ4/6ATe9DJMpZPeiyTMfftGrA/view?utm_content=DAGBi8OQfQ4&utm_campaign=designshare&utm_medium=link&utm_source=editor",
         },
         {
-            name: "My-Figma-Website",
-            index: 3,
-            content: `<p>This is a clone of a front-end of a website in figma. <ul><li>I was asked to build this when I applied for an internship in React.js </li><li>This frontend project got me selected in that screening round.</li><li>The details of the design I was asked for are <a href="https://www.figma.com/file/392G67iWd8t6WAWy6wDMPt/Basic-Responsiveness-Task?type=design&node-id=1-4235&mode=design" target="blank">here</a></li></p>`,
-            github_link: "https://github.com/Giribabi/figma-app",
-            deployment_link: "https://figma-app-one.vercel.app/",
+            name: "ShopEasy",
+            index: 1,
+            content: `<p>This is an front-end project of an e-commerce platform and is completely designed in ReactJs.<ul> <li> The login username and password can be found in dummyjson docs. </li><li>I have already set the login credentials for direct login.</li><li>We must remember that accurate login credentials are necessary to enter into the home page. </li><li>After the login authentication, you will be redirected to the protected Home page. </li><li>Then you can search for the available products. </li><li>You can filter the products according to their price. </li><li>You can view the number of items added to cart at the top of the cart icon. </li><li>The total cost of the items in the cart is found below the cart icon. </li><li>You can view the cart by clicking on the cart icon. </li><li>You can also tap on any item card on the home page to view its complete description. </li><li>At last you can logout from the session by clicking the logout button. <li>I have collected and used all the data from <a href="https://dummyjson.com/docs" target="blank">here</a></li></ul></p>`,
+            github_link: "https://github.com/Giribabi/ShopEasy",
+            deployment_link: "https://shop-easy-updated.vercel.app/",
         },
         {
             name: "UserProfiles",
             index: 4,
-            content: `<p>This is a frondend project to recieve and display data from an API. <ul><li>I was asked to fetch data(user profiles) from an API and show the details of a particular user when clicked.</li><li>I have implemented pagination for the first time through this project.</li><li>I have collected the data from <a href="https://602e7c2c4410730017c50b9d.mockapi.io/users" target="blank">here</a></li></p>`,
+            content: `<p>This is a frontend project to recieve and display data from an API. <ul><li>I was asked to fetch data(user profiles) from an API and show the details of a particular user when clicked.</li><li>I have implemented pagination for the first time through this project.</li><li>I have collected the data from <a href="https://602e7c2c4410730017c50b9d.mockapi.io/users" target="blank">here</a></li></p>`,
             github_link: "github.com/Giribabi/figma-new-task2",
             deployment_link: "https://figma-new-task2.vercel.app/",
         },
@@ -87,6 +91,15 @@ function Projects() {
                                     >
                                         Watch how it looks when deployed
                                     </a>
+                                    {project.ppt_link && (
+                                        <a
+                                            className="link"
+                                            href={project.ppt_link}
+                                            target="blank"
+                                        >
+                                            View Presentation
+                                        </a>
+                                    )}
                                 </div>
                                 <div className="toggle-button-container">
                                     <div
